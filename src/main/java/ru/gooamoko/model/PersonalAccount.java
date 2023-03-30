da07173f1f24d1681e411507f0de2eed1ee14456
Voronin Leonid <gooamoko@rambler.ru>
2023-03-30 15:55:21 +0700

    Пример 5. Взаимоблокировка

---
+++ src/main/java/ru/gooamoko/model/PersonalAccount.java
@@ -0,0 +1,74 @@
+package ru.gooamoko.model;
+
+import java.math.BigDecimal;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Класс упрощенного лицевого счета.
+ */
+public class PersonalAccount {
+    private final Lock accountLock = new ReentrantLock();
+    private BigDecimal fund; // Сумма на счете
+
+    public PersonalAccount(BigDecimal fund) {
+        this.fund = fund;
+    }
+
+    public PersonalAccount() {
+        this.fund = BigDecimal.ZERO;
+    }
+
+    /**
+     * Зачисление средств на счет.
+     */
+    public BigDecimal add(BigDecimal amount) {
+        try {
+            accountLock.lock();
+            fund = fund.add(amount);
+        } finally {
+            accountLock.unlock();
+        }
+
+        return fund;
+    }
+
+    /**
+     * Списание средств со счета.
+     */
+    public BigDecimal subtract(BigDecimal amount) {
+        try {
+            accountLock.lock();
+            if (fund.compareTo(amount) < 0) {
+                throw new RuntimeException("Fund to low to subtract " + amount.toString() + ".");
+            }
+            fund = fund.subtract(amount);
+        } finally {
+            accountLock.unlock();
+        }
+        return fund;
+    }
+
+    /**
+     * Переводим с одного счета на другой.
+     */
+    public BigDecimal transferFrom(PersonalAccount from, BigDecimal amount) {
+        try {
+            accountLock.lock();
+            from.subtract(amount);
+            fund = fund.add(amount);
+        } finally {
+            accountLock.unlock();
+        }
+        return fund;
+    }
+
+    /**
+     * Возвращает количество средств на счете.
+     *
+     * @return количество средств.
+     */
+    public BigDecimal getFund() {
+        return fund;
+    }
+}
---
+++ src/test/java/ru/gooamoko/model/PersonalAccountTest.java
@@ -0,0 +1,73 @@
+package ru.gooamoko.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonalAccountTest {
+
+    @Test
+    public void testAddConcurrent() throws InterruptedException {
+        PersonalAccount testAccount = new PersonalAccount();
+
+        int count = 10;
+        CountDownLatch latch = new CountDownLatch(count);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        for (int i = 0; i < count; i++) {
+            executorService.submit(() -> {
+                testAccount.add(new BigDecimal("100.00"));
+                latch.countDown();
+            });
+        }
+
+        latch.await();
+        assertEquals(new BigDecimal("1000.00"), testAccount.getFund());
+    }
+
+    @Test
+    public void testSubtractConcurrent() throws InterruptedException {
+        PersonalAccount testAccount = new PersonalAccount(new BigDecimal("1000.00"));
+
+        int count = 10;
+        CountDownLatch latch = new CountDownLatch(count);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        for (int i = 0; i < count; i++) {
+            executorService.submit(() -> {
+                testAccount.subtract(new BigDecimal("100.00"));
+                latch.countDown();
+            });
+        }
+
+        latch.await();
+        assertEquals(new BigDecimal("0.00"), testAccount.getFund());
+    }
+
+    @Test
+    public void testTransferConcurrent() throws InterruptedException {
+        PersonalAccount firstAccount = new PersonalAccount(new BigDecimal("1000.00"));
+        PersonalAccount secondAccount = new PersonalAccount(new BigDecimal("1000.00"));
+
+        int count = 10;
+        CountDownLatch latch = new CountDownLatch(count * 2);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        for (int i = 0; i < count; i++) {
+            executorService.submit(() -> {
+                firstAccount.transferFrom(secondAccount, new BigDecimal("100.00"));
+                latch.countDown();
+            });
+            executorService.submit(() -> {
+                secondAccount.transferFrom(firstAccount, new BigDecimal("100.00"));
+                latch.countDown();
+            });
+        }
+
+        latch.await();
+        assertEquals(new BigDecimal("1000.00"), firstAccount.getFund());
+        assertEquals(new BigDecimal("1000.00"), secondAccount.getFund());
+    }
+}
